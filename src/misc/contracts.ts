30167bc521a7dcdae567368e2dedadd21c8a3af5
0xK3K <0xk3k@protonmail.com>
2024-02-12 01:14:47 +0700

    Ready for mint

--- src/misc/contracts.ts
+++ src/misc/contracts.ts
@@ -4,8 +4,8 @@ export const getContracts = (chain: Chain) =>
   ({
     mainnet: {
       eth: '0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7',
-      grails: '0x0',
-      mint: '0x0'
+      grails: '0x2a819b93cc69b45ee5d1a1bfc16954c16f6d35c3873a06c97b95c009bfe502b',
+      mint: '0x6f5b2f4f89542a80ccef4089db49b34bf25086e1f93dc09cf6a7c68498dccc4'
     },
     goerli: {
       eth: '0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7',
