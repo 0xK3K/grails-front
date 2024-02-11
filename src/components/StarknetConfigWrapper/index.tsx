89a84f26a597440c462a90dfaf197dd1c84066f9
0xK3K <0xk3k@protonmail.com>
2024-02-12 01:37:05 +0700

    WIP

--- src/components/StarknetConfigWrapper/index.tsx
+++ src/components/StarknetConfigWrapper/index.tsx
@@ -23,7 +23,7 @@ export default function StarknetConfigWrapper({ children }: { children: ReactNod
           if (chain.id === mainnet.id) {
             return {
               chainId: constants.StarknetChainId.SN_MAIN,
-              nodeUrl: `https://starknet-goerli.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_STARKNET_ALCHEMY_KEY}`
+              nodeUrl: `https://starknet-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_STARKNET_ALCHEMY_KEY}`
             }
           } else if (chain.id === goerli.id) {
             return {
