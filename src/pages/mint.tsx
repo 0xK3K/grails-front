bfd867deca8adebb4da16e32beff8f3e433f49dc
0xK3K <0xk3k@protonmail.com>
2024-02-12 11:38:53 +0700

    mint done

--- src/pages/mint.tsx
+++ src/pages/mint.tsx
@@ -1,30 +1,12 @@
 import { Box, Container, MainText } from '@/components/Layout'
-import { useDispatch } from '@/hooks'
-import { addPendingTransaction } from '@/store/appSlice'
-import { abis, formatEther, getContracts, serializeAddress, serializeU256, toast } from '@/misc'
-import { TransactionType } from '@/types'
-import { Button } from '@nextui-org/react'
-import { useAccount, useContractRead, useContractWrite, useNetwork } from '@starknet-react/core'
+import { useAccount } from '@starknet-react/core'
 import Image from 'next/image'
-import { useCallback, useEffect, useMemo, useState } from 'react'
-import { Call, num } from 'starknet'
+import { useEffect, useMemo, useState } from 'react'
 
 export default function Mint() {
-  const { address, isConnected } = useAccount()
-  const dispatch = useDispatch()
-  const { chain } = useNetwork()
+  const { isConnected } = useAccount()
 
   const [number, setNumber] = useState(0)
-
-  const { data: balanceOf, isLoading } = useContractRead({
-    address: getContracts(chain)!.grails,
-    abi: abis.grails,
-    args: [serializeAddress(getContracts(chain)!.mint)],
-    enabled: !!address,
-    functionName: 'balanceOf'
-  })
-
-  const remaining = useMemo(() => (balanceOf ? ((balanceOf as bigint) / BigInt(10 ** 18)).toString() : 0), [balanceOf])
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -51,40 +33,6 @@ export default function Mint() {
     }
   }, [number])
 
-  const calls = useMemo(() => {
-    if (address) {
-      const contracts = getContracts(chain)!
-
-      try {
-        const approve: Call = {
-          contractAddress: contracts.eth,
-          entrypoint: 'approve',
-          calldata: [serializeAddress(contracts.mint), ...serializeU256(formatEther(0.01))]
-        }
-
-        const mint: Call = {
-          contractAddress: contracts.mint,
-          entrypoint: 'mint',
-          calldata: []
-        }
-
-        return [approve, mint]
-      } catch (error) {
-        console.error('Failed to generate call data', error)
-      }
-    }
-  }, [address, chain])
-
-  const { writeAsync } = useContractWrite({ calls })
-
-  const handleCTA = useCallback(async () => {
-    try {
-      const { transaction_hash: hash } = await writeAsync()
-      toast({ action: TransactionType.Mint, chain, transactionHash: hash, type: 'info' })
-      dispatch(addPendingTransaction({ action: TransactionType.Mint, hash: num.toStorageKey(hash) }))
-    } catch (e) {}
-  }, [chain, dispatch, writeAsync])
-
   return (
     <Container className='h-[100%] max-w-[1400px]'>
       <Box center className='min-h-[70vh]'>
@@ -93,27 +41,13 @@ export default function Mint() {
         ) : (
           <Box col center>
             <Box col center>
-              <MainText heading>the initiation quest has begun</MainText>
-              <MainText heading>each item requires a tithe of 0.01 ether,</MainText>
-              <MainText heading>a token of thy reverence to the realm</MainText>
-              {isLoading ? (
-                <MainText heading>...</MainText>
-              ) : (
-                <MainText heading className='mt-6'>
-                  there are {remaining?.toString()} remaining items available
-                </MainText>
-              )}
+              <MainText heading>the initiation quest has completed</MainText>
+              <MainText heading>each item now in the hands of an adventurer</MainText>
+              <MainText heading>the realm awaits them on their next quest</MainText>
             </Box>
             <Box className='anim-pulsate my-6'>
               <Image src={`/assets/${item}.png`} alt='item' width={80} height={80} />
             </Box>
-            {Number(remaining?.toString() || 0) > 0 && (
-              <Box center>
-                <Button onClick={handleCTA}>
-                  <MainText heading>mint 1 item</MainText>
-                </Button>
-              </Box>
-            )}
           </Box>
         )}
       </Box>
